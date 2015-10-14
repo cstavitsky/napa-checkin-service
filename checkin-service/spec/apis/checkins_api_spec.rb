28ffea4052fba5a3f5b1685f81580266ef5b9ed8
cstavitsky <corridorr@gmail.com>
2015-10-14 17:49:05 -0500

    add failing api tests for rewards_api, users_api, locations_api, checkins_api

--- checkin-service/spec/apis/checkins_api_spec.rb
+++ checkin-service/spec/apis/checkins_api_spec.rb
@@ -7,10 +7,28 @@ end
 describe CheckinsApi do
   include Rack::Test::Methods
 
-  describe 'e.g. GET, POST, PUT, etc.' do
-    it 'needs tests to be written!' do
-      pending('write tests for CheckinsApi!')
-    end
-  end
+  	context "GET /checkins" do
+        it "returns an empty array of checkins" do
+          get "/checkins"
+          expect(last_response.status).to eq(200)
+          expect(JSON.parse(response.body)).to eq([])
+        end
+      end
+
+      context "GET /checkins/:id" do
+        it "returns a checkin by id" do
+          checkin = Checkin.create!
+          get "/checkins/#{checkin.id}"
+          expect(last_response.body).to eq(checkin.to_json)
+        end
+      end
+
+      context "POST /checkins" do 
+      	it 'creates many checkins' do 
+      		checkins = [{ user_id: 1, location_id: 1, points: 5 }, { user_id: 2, location_id: 2, points: 5 }]
+      		post "/checkins", checkins.to_json, 'CONTENT_TYPE' => 'application/json'
+      		expect(last_response.body).to eq(201)
+      	end
+      end
 
 end
--- checkin-service/spec/apis/locations_api_spec.rb
+++ checkin-service/spec/apis/locations_api_spec.rb
@@ -7,10 +7,27 @@ end
 describe LocationsApi do
   include Rack::Test::Methods
 
-  describe 'e.g. GET, POST, PUT, etc.' do
-    it 'needs tests to be written!' do
-      pending('write tests for LocationsApi!')
-    end
-  end
+      context "GET /locations" do
+        it "returns an empty array of locations" do
+          get "/locations"
+          expect(last_response.status).to eq(200)
+          expect(JSON.parse(response.body)).to eq([])
+        end
+      end
 
+      context "GET /locations/:id" do
+        it "returns a location by id" do
+          location = Location.create!
+          get "/locations/#{location.id}"
+          expect(last_response.body).to eq(location.to_json)
+        end
+      end
+
+      context "POST /locations" do 
+      	it 'creates many locations' do 
+      		locations = [{ store_name: '...' }, { store_name: '...' }]
+      		post "/locations", locations.to_json, 'CONTENT_TYPE' => 'application/json'
+      		expect(last_response.body).to eq(201)
+      	end
+      end
 end
